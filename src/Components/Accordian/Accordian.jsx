c4099689038ac416f0cca74a66698bbcc82115d1
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2024-03-09 12:30:44 +0530

    done with Accordian

--- src/Components/Accordian/Accordian.jsx
+++ src/Components/Accordian/Accordian.jsx
@@ -4,11 +4,33 @@ import data from './data.js'
 
 
 function Accordian() {
-
-
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+
+    // single selection
+
     function handleSingleSelection(currId) {
         setSelected(currId === selected ? null : currId)
+    }
+
+    //Multi Selection
+
+    function handleMultipleSelection(currId) {
+        let copyofMultiple = [...multiple]
+
+        const indexOfCurrId = copyofMultiple.indexOf(currId);
+
+        if (indexOfCurrId == -1) {
+            copyofMultiple.push(currId)
+        }
+        else {
+            copyofMultiple.splice(indexOfCurrId, 1)
+        }
+
+        setMultiple(copyofMultiple)
+        console.log(multiple)
     }
 
     return (
@@ -18,19 +40,34 @@ function Accordian() {
             <div className='w-[500px]'>
                 {/*Question */}
                 {data && data.length > 0 ? data.map(item => (
-                    <div className='mb-2 text-lg font-semibold' onClick={() => handleSingleSelection(item.id)} key={item.id}>
+                    <div className='mb-2 text-lg font-semibold'
+                        onClick={enableMultiSelection ? () => handleMultipleSelection(item.id) : () => handleSingleSelection(item.id)}
+                        key={item.id}>
                         {/*title */}
                         <div className='flex px-2 py-1 border-b-black border-b-2 items-center justify-between cursor-pointer bg-blue-300'>
                             <h3 className=''>{item.question}</h3>
                             {selected && selected === item.id ? <span className='text-2xl'>-</span> : <span className='text-2xl'>+</span>}
                         </div>
-                        {selected && selected === item.id ? <div className='h-auto italic w-auto px-2 py-1  bg-blue-300'>{item.answer}</div> : null}
+                        {
+                            enableMultiSelection ? multiple.indexOf(item.id) !== -1 &&
+                                (<div className='h-auto italic w-auto px-2 py-1  bg-blue-300'>{item.answer}</div>)
+                                : selected === item.id &&
+                                (<div className='h-auto italic w-auto px-2 py-1  bg-blue-300'>{item.answer}</div>)
+                        }
+                        {/*{
+                            selected && selected === item.id ?
+                                <div className='h-auto italic w-auto px-2 py-1  bg-blue-300'>{item.answer}</div>
+                                : null
+                        }*/}
                     </div>
                 ))
                     : <div className=''>Data Not Found</div>
                 }
-
             </div>
+            <button
+                className='bg-black text-white rounded p-1 hover:bg-gray-500'
+                onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+            >Enable Multiple Selection</button>
         </div>
     )
 }
